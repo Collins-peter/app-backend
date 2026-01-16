c8d7c43d70052dd5eb7fd6598e97aab0302b6925
Collins-peter <anicollins09@gmail.com>
2026-01-16 14:09:10 +0100

    updated server.mjs

--- server.mjs
+++ server.mjs
@@ -55,7 +55,7 @@ app.post("/upload-image", verifyToken, uploadImage);
 
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port localhost:${PORT}`);
 })
